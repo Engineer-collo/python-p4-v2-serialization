c8ee2c88917496fdd097a18bce29e176ed73ff7c
Engineer-Collo <collinslikhomba@gmail.com>
2025-03-26 08:41:12 +0300

    seriolization

--- server/app.py
+++ server/app.py
@@ -20,6 +20,38 @@ def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
 
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id==id).first()
 
+    if pet:
+        response_body = pet.to_dict()
+        response_status = 200
+    else:
+        response_body = {'message': f'Pet id {id} not found.'}
+        response_status = 404
+    
+    response = make_response(response_body,response_status)
+    return response
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    #fets from database species
+    pets = Pet.query.filter_by(species=species).all()
+    pets_dict = []
+    for pet in pets:
+        #store pet dictionary
+        pets_dict.append(pet.to_dict())
+     
+    count = len(pets_dict)
+    #response body
+    response_body = {
+        'count' : count,
+        'pets' : pets_dict
+        }
+    response_status = 200
+
+    response = make_response(response_body,response_status)
+    return response
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
